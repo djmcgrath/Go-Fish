e3f2011115a512d395ba7f31aae713de9ba79c16
Daniel McGrath <djmcgrath95@gmail.com>
2023-10-16 11:14:17 -0600

    Bare bones

---
+++ Components/DeckofCards.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DeckofCards = () => {
+  return (
+    <div>DeckofCards</div>
+  )
+}
---
+++ Components/Players Hand/HandContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HandContainer = () => {
+  return (
+    <div>HandContainer</div>
+  )
+}
